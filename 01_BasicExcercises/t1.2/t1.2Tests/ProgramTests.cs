e84d6aab2b0e65951f533190f1c65db687ce8bba
mikko Reini <AD4016@student.jamk.fi>
2023-11-06 09:05:46 +0200

    tehtävien 1.1 -1,3

---
+++ 01_BasicExcercises/t1.2/t1.2Tests/ProgramTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using t1._2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1._2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Tarkista_Että_Vastaus_On_Alle_satayks_Jos_Ei_Niin_Heitä_Poikkeus()
+        {
+            Program rogrammi = new Program();
+            rogrammi.vastaus = "100";
+            try 
+            {
+                rogrammi.Maksimi_Numero(100);
+            }
+            catch(System.ArgumentOutOfRangeException e) 
+            {
+                StringAssert.Contains(e.Message, " numero on yli 100");
+            }
+            
+        }
+        [TestMethod()]
+        public void Testaa_Että_Vastaus_on_Numerollinen()
+        {
+            Program rogrammi = new Program();
+            rogrammi.vastaus= "9";
+            Assert.IsTrue(rogrammi.Vastaus_on_numerollinen(rogrammi.vastaus, 0));
+        }
+    }
+}
---
+++ 01_BasicExcercises/t1/t1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    public class Program
+    {
+        public string vastaus;
+        static void Main(string[] args)
+        {
+            Program rogrammi= new Program();
+            Console.WriteLine("anna numero");
+            int num1 = 0;
+            int num2 = 0;
+            string syöttö;
+            syöttö = Console.ReadLine();
+            try 
+            {
+                num1 = int.Parse(syöttö);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            syöttö = Console.ReadLine();
+            try
+            {
+                num2 = int.Parse(syöttö);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            rogrammi.vastaus = rogrammi.Miinustus(num1, num2).ToString();
+            Console.WriteLine("tulos on " + (rogrammi.Vastaus_on_numerollinen(rogrammi.vastaus, 0) ? rogrammi.vastaus: "ei voi laskea")) ;
+            Console.ReadKey();
+
+
+        }
+        public int Miinustus(int num1, int num2) 
+        {
+            if (num1 > int.MaxValue || num2 > int.MaxValue) 
+            {
+                throw new ArgumentException(int.MaxValue.ToString());
+            }
+            if (num1 < int.MinValue|| num2 < int.MinValue) 
+            {
+                throw new ArgumentException(int.MinValue.ToString());
+            }
+            int minustus = num1 - num2;
+            return minustus;
+        }
+        public bool Vastaus_on_numerollinen(string vastaus, int num) 
+        {
+            if(int.TryParse(vastaus, out num)) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
